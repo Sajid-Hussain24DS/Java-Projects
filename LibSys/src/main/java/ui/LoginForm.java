85f18f6f5f4fa00544c104d9808cd5d0f81dcaa9
Sajid-Hussain <lasharisajidhussain9@gmail.com>
2025-09-21 02:53:04 +0500

    Eommerce Project

--- LibSys/src/main/java/ui/LoginForm.java
+++ LibSys/src/main/java/ui/LoginForm.java
@@ -127,9 +127,7 @@ public class LoginForm extends javax.swing.JFrame {
 
     }//GEN-LAST:event_loginButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
